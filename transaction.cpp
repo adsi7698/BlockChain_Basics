6ca6f528425c6e9441b03156059955d3cb463e5c
adsi7698 <aditya.sinha1997@outlook.com>
2018-07-31 22:21:44 +0530

    Withdraw money from bank in bitcoin started , some bugs to fix

--- transaction.cpp
+++ transaction.cpp
@@ -10,8 +10,9 @@ class person {
         int balance;
     public:
         person() {}
-
         void getName(string name2);
+        string myName();
+        int getBalance();
 };
 
 void person::getName(string name2) {
@@ -19,16 +20,59 @@ void person::getName(string name2) {
     cout<<"Account Created !"<<endl<<"Welcome "<<name2<<" !"<<endl;
 }
 
+string person::myName() {
+    return name;
+}
+
+int person::getBalance() {
+    return balance;
+}
+
+static counting = 0;
+
 class bitcoin {
     private:
+        string a;
+        string b;
         int transaction;
         bitcoin* prev;
         bitcoin* cur;
     public:
-        void withdraw(person p1);
+        bitcoin();
+        void withdraw(person p1, int amount);
         void deposit(person p1);
         void convert(person p1);
 };
+
+void bitcoin::bitcoin() {
+
+    if(counting == 0) {
+        prev = NULL;
+        // cur points to its own location ...
+    }
+
+}
+
+void bitcoin::withdraw(person p1, int amount) {
+
+    if(p1.getBalance() == 0) {
+        cout<<"No balance"<<endl;
+        return ;
+    }
+    else if(p1.getBalance() >= amount) {
+        char choice;
+        cout<<"Do you want to know the current balance ?"<<endl;
+        cout<<"Y/N ?"<<endl;
+        cin>>choice;
+        if(choice == 'Y') {
+            cout<<"Your balance is "<<p1.getBalance()<<"Mr. "<<p1.myName()<<endl;
+        }
+    }
+    else {
+        cout<<"Not enough balance !"<<endl;
+        cout<<"Transaction failed !"<<endl;
+    }
+}
 
 int main() {
 
@@ -53,14 +97,16 @@ int main() {
                     break;
             case 2: cout<<"Enter Amount to withdraw !"<<endl;
                     cin>>amount;
-
+                    bitcoin b1;
+                    b1.withdraw(a[i], amount);
+                    break;
             default: return 0;
         }
 
         cout<<endl<<endl;
         cout<<"What would you like to do in this blockchain world ?"<<endl;
         cout<<"1. Create Account"<<endl<<"2. Withdraw Money"<<endl<<"3. Deposit Money"<<endl<<"4. Delete Account"<<endl;
-        cout<<"5. Convert from bitcoin to inr"<<endl<<"6. Current Balance "<<endl;
+        cout<<"5. Convert from Bitcoin to INR"<<endl<<"6. Current Balance "<<endl;
         cout<<"Warning :: only 10 accounts can be created at once !!"<<endl;
         cout<<endl;
     }
